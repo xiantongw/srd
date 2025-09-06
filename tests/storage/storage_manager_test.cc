0be832f972366653406326d2d132399821f2000e
Xiantong Wang <xtwang1996@gmail.com>
2025-09-06 16:55:20 -0400

    testing auto ci

--- tests/storage/storage_manager_test.cc
+++ tests/storage/storage_manager_test.cc
@@ -10,7 +10,7 @@ static std::string temp_path() {
     return std::string("srd_sm_test_") + std::to_string(::getpid()) + ".dat";
 }
 
-TEST(StorageManagerTest, CREAT) {
+TEST(StorageManagerTest, CREATION) {
     std::string path = temp_path();
     StorageManager sm(path);
     EXPECT_EQ(sm.num_pages(), 1u);
