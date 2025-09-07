d47e4247321bf96112aeacb29cde6cdd3e9c43ab
Tom Wang <xtwang1996@gmail.com>
2025-09-07 00:38:26 -0400

    add linting

--- tests/storage/storage_manager_test.cc
+++ tests/storage/storage_manager_test.cc
@@ -10,10 +10,11 @@ static std::string temp_path() {
     return std::string("srd_sm_test_") + std::to_string(::getpid()) + ".dat";
 }
 
-TEST(StorageManagerTest, CREATION) {
-    std::string path = temp_path();
-    StorageManager sm(path);
-    EXPECT_EQ(sm.num_pages(), 1u);
-    EXPECT_LE(sm.num_pages(), 2u);
+TEST(StorageManagerTest, CreateAndExtend) {
+    auto path = temp_path();
+    {
+        StorageManager sm(path);
+        EXPECT_GE(sm.num_pages(), 1u);
+    }
     std::remove(path.c_str());
 }
