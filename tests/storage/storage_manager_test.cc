6bb654ffaef528a54a259a405f199c160dfce4d3
Tom Wang <xtwang1996@gmail.com>
2025-09-07 00:56:36 -0400

    Revert "test ci"
    This reverts commit 52e0701e6e74ebfb723f44ec47607cd15eb21e3e.

--- tests/storage/storage_manager_test.cc
+++ tests/storage/storage_manager_test.cc
@@ -10,11 +10,9 @@ static std::string temp_path() {
     return std::string("srd_sm_test_") + std::to_string(::getpid()) + ".dat";
 }
 
-TEST(StorageManagerTest, CreateAndExtend) {
-    auto path = temp_path();
-    {
-        StorageManager sm(path);
-        EXPECT_GE(sm.num_pages(), 1u);
-    }
+TEST(StorageManagerTest, CREATION) {
+    std::string path = temp_path();
+    StorageManager sm(path);
+    EXPECT_EQ(sm.num_pages(), 1u);
     std::remove(path.c_str());
 }
